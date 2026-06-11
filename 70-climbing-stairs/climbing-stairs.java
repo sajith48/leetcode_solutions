4298279f0fc81f026dc7268404823a17c61b55d1
SAJITH R <sajithr.24cse@kongu.edu>
2026-06-11 11:14:05 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (52.86%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,13 +1,22 @@
+import java.util.Arrays;
+
 class Solution {
     public int climbStairs(int n) {
-        if (n <= 2)
-            return n;
-        int a = 1, b = 2;
-        for (int i = 3; i <= n; i++) {
-            int c = a + b;
-            a = b;
-            b = c;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return check(n, dp);
+    }
+    
+    public int check(int n, int[] dp) {
+        if (n == 0 || n == 1) {
+            return 1; 
         }
-        return b;
+        
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        
+        dp[n] = check(n - 1, dp) + check(n - 2, dp);
+        return dp[n];
     }
 }
