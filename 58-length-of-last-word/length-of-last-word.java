74274d5a6e82bec1fa77da60c19c89bf7107a1db
SAJITH R <sajithr.24cse@kongu.edu>
2026-06-11 21:05:32 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (16.76%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,6 +1,11 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] arr = s.split(" ");
-        return arr[arr.length-1].length();
+        String str = s.trim();
+        int count = 0;
+        for(int i=str.length()-1; i>=0; i--){ 
+            if(str.charAt(i)!=' ') count++;
+            else break;
+        }
+        return count;
     }
 }
