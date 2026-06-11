dfb953399240dbadb835820cbd37798c5c34ce52
SAJITH R <sajithr.24cse@kongu.edu>
2026-06-11 20:39:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (66.64%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String toLowerCase(String s) {
+        char[] arr = s.toCharArray();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>='A' && arr[i]<='Z')arr[i]|=' ';
+        }
+        return new String(arr);
+    }
+}
