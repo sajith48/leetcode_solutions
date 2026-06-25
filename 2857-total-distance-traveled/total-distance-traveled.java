88d0336a26ddbb179a73c775051a804d11a25ad6
SAJITH R <sajithr.24cse@kongu.edu>
2026-06-25 21:24:22 +0530

    Time: 4 ms (100.00%) | Memory: 46 MB (48.76%) - LeetSync

---
+++ 2857-total-distance-traveled/total-distance-traveled.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int distanceTraveled(int m, int a) {
+        int totalfuel = 0;
+        while(m>=5){
+            m-=5;
+            totalfuel+=5;
+            if(a>=1){
+                a-=1;
+                m++;
+            }
+        }
+        totalfuel+=m;
+        return 10*totalfuel;
+    }
+}
