54631b3896337ed43a397c0e83eeef922d3eaf87
SAJITH R <sajithr.24cse@kongu.edu>
2026-06-25 21:09:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (88.05%) - LeetSync

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        int step = 0;
+        while(num!=0){
+            step++;
+            if(num%2==0)num/=2;
+            else num--;
+        }
+        return step;
+    }
+}
