4208dc67182d4b1a4ff4b645edde8ace24f7eb08
SAJITH R <sajithr.24cse@kongu.edu>
2026-07-01 21:47:27 +0530

    Time: 5 ms (68.58%) | Memory: 46 MB (47.15%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int n = 0,x = 0;
+        for(char ch : moves.toCharArray()){
+            if(ch=='U')n+=1;
+            else if(ch=='D')n-=1;
+            else if(ch=='L')x+=1;
+            else if(ch=='R')x-=1;
+        }
+            return n==0 && x==0;
+    }
+}
