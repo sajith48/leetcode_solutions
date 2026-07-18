d5f2da72b2458092a94de88dbd22aa33e27508eb
SAJITH R <sajithr.24cse@kongu.edu>
2026-07-18 21:26:37 +0530

    Create 1013-FibonacciNumber

---
+++ https:/github.com/sajith48/leetcode_solutions.git/1013-FibonacciNumber/1013-FibonacciNumber.java
@@ -0,0 +1,16 @@
+// Last updated: 7/18/2026, 9:26:36 PM
+class Solution {
+    public int fib(int n) {
+        if(n==0) return 0;
+        if(n==1) return 1;
+       int a = 0;
+       int b = 1;
+      
+       for(int i =2; i<=n; i++) {
+        int c = a +b;
+        a = b;
+        b = c;  
+        }
+        return b;   
+    }
+}
