6395c3814bcab6bc84cc5c517619a9d98e613009
SAJITH R <sajithr.24cse@kongu.edu>
2026-07-18 21:26:26 +0530

    Create 1128-RemoveAllAdjacentDuplicatesInString

---
+++ https:/github.com/sajith48/leetcode_solutions.git/1128-RemoveAllAdjacentDuplicatesInString/1128-RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,15 @@
+// Last updated: 7/18/2026, 9:26:25 PM
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> st = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for(char a: s.toCharArray()){
+            if(!st.isEmpty() && a == st.peek())st.pop();
+            else st.push(a);
+        }
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        return sb.reverse().toString();
+    }
+}
