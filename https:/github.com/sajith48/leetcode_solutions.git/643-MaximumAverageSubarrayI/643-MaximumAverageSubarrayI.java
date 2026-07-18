d04e965b958983d6e97a262b015730031e6ef56b
SAJITH R <sajithr.24cse@kongu.edu>
2026-07-18 21:27:19 +0530

    Create 643-MaximumAverageSubarrayI

---
+++ https:/github.com/sajith48/leetcode_solutions.git/643-MaximumAverageSubarrayI/643-MaximumAverageSubarrayI.java
@@ -0,0 +1,18 @@
+// Last updated: 7/18/2026, 9:27:19 PM
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        double currentSum = 0;
+        for (int i = 0; i < k; i++) {
+            currentSum += nums[i];
+        }
+        
+        double maxSum = currentSum;
+        
+        for (int i = k; i < nums.length; i++) {
+            currentSum += nums[i] - nums[i - k];
+            maxSum = Math.max(maxSum, currentSum);
+        }
+        
+        return maxSum / k;
+    }
+}
