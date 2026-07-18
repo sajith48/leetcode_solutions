2fcd2a003848f2b606e22d48708cf3bbf5a1656d
SAJITH R <sajithr.24cse@kongu.edu>
2026-07-18 21:26:51 +0530

    Create 801-IsGraphBipartite

---
+++ https:/github.com/sajith48/leetcode_solutions.git/801-IsGraphBipartite/801-IsGraphBipartite.java
@@ -0,0 +1,32 @@
+// Last updated: 7/18/2026, 9:26:50 PM
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] color = new int[n];
+        Arrays.fill(color, -1);
+        
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {
+                Queue<Integer> queue = new LinkedList<>();
+                queue.add(i);
+                color[i] = 0; 
+                
+                while (!queue.isEmpty()) {
+                    int curr = queue.poll();
+                    
+                    for (int neighbor : graph[curr]) {
+                       
+                        if (color[neighbor] == -1) {
+                            color[neighbor] = 1 - color[curr];
+                            queue.add(neighbor);
+                        } 
+                        else if (color[neighbor] == color[curr]) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
