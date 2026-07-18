b828b42c3c80d52cc4f105efac5beaf0dd49588a
SAJITH R <sajithr.24cse@kongu.edu>
2026-07-18 21:25:26 +0530

    Create 2210-FindTargetIndicesAfterSortingArray

---
+++ https:/github.com/sajith48/leetcode_solutions.git/2210-FindTargetIndicesAfterSortingArray/2210-FindTargetIndicesAfterSortingArray.java
@@ -0,0 +1,11 @@
+// Last updated: 7/18/2026, 9:25:26 PM
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<Integer> al = new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target)al.add(i);
+        }
+        return al;
+    }
+}
