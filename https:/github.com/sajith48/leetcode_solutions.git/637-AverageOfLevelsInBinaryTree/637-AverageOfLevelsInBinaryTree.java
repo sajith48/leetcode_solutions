e8932772355fcef25e4cd0c91103e6c0762b035e
SAJITH R <sajithr.24cse@kongu.edu>
2026-07-18 21:27:20 +0530

    Create 637-AverageOfLevelsInBinaryTree

---
+++ https:/github.com/sajith48/leetcode_solutions.git/637-AverageOfLevelsInBinaryTree/637-AverageOfLevelsInBinaryTree.java
@@ -0,0 +1,47 @@
+// Last updated: 7/18/2026, 9:27:20 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            double levelSum = 0;
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                levelSum += currentNode.val;
+
+                if (currentNode.left != null) {
+                    queue.add(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.add(currentNode.right);
+                }
+            }
+            result.add(levelSum / levelSize);
+        }
+
+        return result;
+    }
+}
