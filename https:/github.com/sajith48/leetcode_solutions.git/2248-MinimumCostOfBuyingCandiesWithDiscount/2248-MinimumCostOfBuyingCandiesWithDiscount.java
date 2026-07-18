1171b05357db172f1cae662276076835f9cd3ea6
SAJITH R <sajithr.24cse@kongu.edu>
2026-07-18 21:25:19 +0530

    Create 2248-MinimumCostOfBuyingCandiesWithDiscount

---
+++ https:/github.com/sajith48/leetcode_solutions.git/2248-MinimumCostOfBuyingCandiesWithDiscount/2248-MinimumCostOfBuyingCandiesWithDiscount.java
@@ -0,0 +1,14 @@
+// Last updated: 7/18/2026, 9:25:19 PM
+class Solution {
+    public int minimumCost(int[] cost) {
+        int c = 0;
+        Arrays.sort(cost);
+        for(int i=cost.length-1;i>=0;i--){
+            c+=cost[i];
+            i--;
+            if(i>=0)c+=cost[i];
+            i--;
+        }
+        return c;
+    }
+}
