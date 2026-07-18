a38dfc4a1aeaa524356ad717de2bb7ab5e8f6676
SAJITH R <sajithr.24cse@kongu.edu>
2026-07-18 21:27:23 +0530

    Create 543-DiameterOfBinaryTree

---
+++ https:/github.com/sajith48/leetcode_solutions.git/543-DiameterOfBinaryTree/543-DiameterOfBinaryTree.java
@@ -0,0 +1,35 @@
+// Last updated: 7/18/2026, 9:27:23 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private int maxDiameter = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDiameter = 0; 
+        calculateHeight(root);
+        return maxDiameter;
+    }
+
+    private int calculateHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = calculateHeight(node.left);
+        int rightHeight = calculateHeight(node.right);
+        maxDiameter = Math.max(maxDiameter, leftHeight + rightHeight);
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+}
