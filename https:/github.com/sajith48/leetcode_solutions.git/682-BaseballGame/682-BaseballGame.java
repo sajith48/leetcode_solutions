13207a762b0eef9eb10423d639a02e78992c1383
SAJITH R <sajithr.24cse@kongu.edu>
2026-07-18 21:27:14 +0530

    Create 682-BaseballGame

---
+++ https:/github.com/sajith48/leetcode_solutions.git/682-BaseballGame/682-BaseballGame.java
@@ -0,0 +1,28 @@
+// Last updated: 7/18/2026, 9:27:14 PM
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> scoreStack = new Stack<>();
+
+        for (String op : operations) {
+            if (op.equals("+")) {
+                int lastScore = scoreStack.pop();
+                int newScore = lastScore + scoreStack.peek();
+                scoreStack.push(lastScore);
+                scoreStack.push(newScore);
+            } else if (op.equals("D")) {
+                scoreStack.push(scoreStack.peek() * 2);
+            } else if (op.equals("C")) {
+                scoreStack.pop();
+            } else {
+                scoreStack.push(Integer.parseInt(op));
+            }
+        }
+
+        int totalSum = 0;
+        for (int score : scoreStack) {
+            totalSum += score;
+        }
+
+        return totalSum;
+    }
+}
