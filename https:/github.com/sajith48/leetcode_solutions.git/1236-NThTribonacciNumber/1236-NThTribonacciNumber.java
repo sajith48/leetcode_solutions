224da7e0bb4be05ffdfa73fcd303a67ae55bfedd
SAJITH R <sajithr.24cse@kongu.edu>
2026-07-18 21:26:21 +0530

    Create 1236-NThTribonacciNumber

---
+++ https:/github.com/sajith48/leetcode_solutions.git/1236-NThTribonacciNumber/1236-NThTribonacciNumber.java
@@ -0,0 +1,17 @@
+// Last updated: 7/18/2026, 9:26:21 PM
+class Solution {
+    public int tribonacci(int n) {
+        int[] memo = new int[n + 1];
+        return helper(n, memo);
+    }
+    
+    private int helper(int n, int[] memo) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+        
+        if (memo[n] != 0) return memo[n];
+        
+        memo[n] = helper(n - 1, memo) + helper(n - 2, memo) + helper(n - 3, memo);
+        return memo[n];
+    }
+}
