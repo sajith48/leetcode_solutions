eaf5b5176307d9f506233fb96e4ad9d0b36b6b8f
SAJITH R <sajithr.24cse@kongu.edu>
2026-07-18 21:24:21 +0530

    Create 4307-ExactlyOneConsecutiveSetBitsPair

---
+++ https:/github.com/sajith48/leetcode_solutions.git/4307-ExactlyOneConsecutiveSetBitsPair/4307-ExactlyOneConsecutiveSetBitsPair.java
@@ -0,0 +1,11 @@
+// Last updated: 7/18/2026, 9:24:21 PM
+class Solution {
+    public boolean consecutiveSetBits(int n) {
+        int count=0;
+        while(n>0){
+            if((n&3)==3)count++;
+            n>>=1;
+        }
+        return count==1;
+    }
+}
