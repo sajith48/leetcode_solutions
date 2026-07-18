9099000fa7be22cb4d01fbdf342edcdac2eeef92
SAJITH R <sajithr.24cse@kongu.edu>
2026-07-18 21:26:54 +0530

    Create 784-InsertIntoABinarySearchTree

---
+++ https:/github.com/sajith48/leetcode_solutions.git/784-InsertIntoABinarySearchTree/784-InsertIntoABinarySearchTree.java
@@ -0,0 +1,30 @@
+// Last updated: 7/18/2026, 9:26:54 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root==null)return new TreeNode(val);
+            if(val <=root.val){
+                TreeNode curr = insertIntoBST(root.left,val);
+                root.left = curr;
+            }
+            else {
+                TreeNode curr = insertIntoBST(root.right,val);
+                root.right = curr;
+            }
+        return root;
+    }
+}
