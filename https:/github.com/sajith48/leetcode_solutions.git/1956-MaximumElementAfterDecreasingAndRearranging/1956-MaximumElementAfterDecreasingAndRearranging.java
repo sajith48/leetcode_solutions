766d78e601da0cdc1ede958e3629c7651d77d883
SAJITH R <sajithr.24cse@kongu.edu>
2026-07-18 21:25:46 +0530

    Create 1956-MaximumElementAfterDecreasingAndRearranging

---
+++ https:/github.com/sajith48/leetcode_solutions.git/1956-MaximumElementAfterDecreasingAndRearranging/1956-MaximumElementAfterDecreasingAndRearranging.java
@@ -0,0 +1,18 @@
+// Last updated: 7/18/2026, 9:25:45 PM
+class Solution {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        int n = arr.length;
+        int[] counts = new int[n + 1];
+        
+        for (int num : arr) {
+            counts[Math.min(num, n)]++;
+        }
+        
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            ans = Math.min(ans + counts[i], i);
+        }
+        
+        return ans;
+    }
+}
