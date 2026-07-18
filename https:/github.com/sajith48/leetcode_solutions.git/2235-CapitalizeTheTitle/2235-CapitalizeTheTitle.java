f08a3726c983072c59aa1fcf370e72a6c53b11f8
SAJITH R <sajithr.24cse@kongu.edu>
2026-07-18 21:25:23 +0530

    Create 2235-CapitalizeTheTitle

---
+++ https:/github.com/sajith48/leetcode_solutions.git/2235-CapitalizeTheTitle/2235-CapitalizeTheTitle.java
@@ -0,0 +1,14 @@
+// Last updated: 7/18/2026, 9:25:22 PM
+class Solution {
+    public String capitalizeTitle(String title) {
+        String[] words = title.toLowerCase().split(" ");
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() > 2) {
+                char[] chars = words[i].toCharArray();
+                chars[0] = Character.toUpperCase(chars[0]);
+                words[i]  = new String(chars);
+            }
+        }
+        return String.join(" ", words);
+    }
+}
