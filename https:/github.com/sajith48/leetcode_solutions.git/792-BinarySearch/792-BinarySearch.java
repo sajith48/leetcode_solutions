6bf59dcf00254225ddfdc9539b168feb023f4ee1
SAJITH R <sajithr.24cse@kongu.edu>
2026-07-18 21:26:56 +0530

    Create 792-BinarySearch

---
+++ https:/github.com/sajith48/leetcode_solutions.git/792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,14 @@
+// Last updated: 7/18/2026, 9:26:55 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]<target)left = mid+1;
+            else right = mid-1;
+        }
+        return -1;
+    }
+}
